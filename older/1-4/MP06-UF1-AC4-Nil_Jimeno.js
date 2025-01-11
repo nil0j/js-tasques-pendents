3da7b9e85648c82ccc8ea61004b8f95bff0a3cb3
niliara-edu <nil.jimeno@estudiant.fjaverianas.com>
2025-01-11 15:50:20 +0100

    init

---
+++ older/1-4/MP06-UF1-AC4-Nil_Jimeno.js
@@ -0,0 +1,139 @@
+// DONE //
+
+function createList() {
+    let ul = document.createElement("ul")
+    
+    let json = `{
+        "Birds": {},
+        "Potatoes": {},
+        "Eggs": {}
+    }`
+    
+    let object = JSON.parse(json)
+
+    
+    Object.entries(object).forEach(entry => {
+        let li = document.createElement("li")
+        li.innerHTML = entry[0]
+        ul.appendChild(li)
+    })
+
+    document.body.appendChild(ul)
+}
+
+
+function createList2() {
+    let ul = document.createElement("ul")
+    let json = `{
+        "Birds": {
+            "Flying": {},
+            "Not flying": {}
+        },
+        "Potatoes": {},
+        "Eggs": {
+            "Fried": {},
+            "Boiled": {}
+        }
+    }`
+    let object = JSON.parse(json)
+    
+    Object.entries(object).forEach(entry => {
+        let li = document.createElement("li")
+        li.innerHTML = entry[0]
+
+        if (Object.entries(entry[1]).length > 0) {
+            let innerUl = document.createElement("ul")
+
+            Object.entries(entry[1]).forEach(entry => {
+                let innerLi = document.createElement("li")
+                innerLi.innerHTML = entry[0]
+                innerUl.append(innerLi)
+            })
+
+            li.appendChild(innerUl)
+        }
+        
+        ul.appendChild(li)
+    })
+
+    document.body.appendChild(ul)
+}
+
+function updateList(json, color) {
+    if (document.getElementById("list") != null) {
+        document.getElementById("list").remove()
+    }
+
+    let object = JSON.parse(json)
+    let div = document.createElement("div")
+    div.id = "list"
+    div.style.color = color
+
+    function clickEffects(li, ul) {
+        if (li.style.color != "yellow") {
+            li.style.color = "yellow"
+        } else {
+            li.style.color = color
+        }
+
+        const clone = li.cloneNode(true)
+        clone.addEventListener("click", _ => clickEffects(clone, ul))
+        clone.addEventListener("contextmenu", _ => clone.remove())
+        ul.appendChild(clone)
+    }
+
+    function newUl(obj) {
+        let ul = document.createElement("ul")
+        ul.style.color = color
+        Object.entries(obj).forEach(entry => {
+            let li = document.createElement("li")
+            li.innerHTML = entry[0]
+
+            if (Object.entries(entry[1]).length > 0) {
+                let innerUl = newUl(entry[1])
+                li.appendChild(innerUl)
+            } else {
+                li.addEventListener("click", _ => clickEffects(li, ul))
+                li.addEventListener("contextmenu", _ => li.remove())
+            }
+            
+            ul.appendChild(li)
+        })
+        return ul
+    }
+
+    let ul = newUl(object)
+    div.appendChild(ul)
+    document.body.appendChild(div)
+}
+
+function printUl() {
+}
+
+
+let initialJson = `{
+    "Birds": {
+        "Flying": {},
+        "Not flying": {}
+    },
+    "Potatoes": {},
+    "Eggs": {
+        "Fried": {},
+        "Boiled": {}
+    }
+}`
+
+let textarea = document.createElement("textarea")
+textarea.value = initialJson
+document.body.appendChild(textarea)
+
+let colorInput = document.createElement("input")
+colorInput.type = "color"
+document.body.appendChild(colorInput)
+
+let convert = document.createElement("button")
+convert.innerText = "Convert"
+convert.addEventListener("click", _ => {
+    updateList(textarea.value, colorInput.value)
+})
+document.body.appendChild(convert)
---
+++ older/1-5/ac5.js
@@ -0,0 +1,165 @@
+// 1. Saca por consola todo el contenido del body sin utilizar ningún método de document
+let html;
+
+async function getData(callback) {
+    await fetch("ac5.html")
+        .then(response => response.text())
+        .then((htmlContent) => {
+            const parser = new DOMParser()
+            const dom = parser.parseFromString(htmlContent, 'text/html')
+            const bodyContent = dom.querySelector('body').innerHTML
+            return bodyContent
+        })
+        .then(data => callback(data))
+        .catch(error => console.error(error))
+}
+getData((data) => {
+    console.log("ex 1", data)
+})
+
+// 2. Saca por consola el texto del elemento #titulo sin utilizar ningún método de document
+console.log("ex 2", titulo)
+
+// 3. Saca el contenido del tercer li usando el método querySelector
+console.log("ex 3", document.querySelector('ul > li'))
+
+// 4. Saca el contenido del tercer li usando el método querySelectorAll
+console.log("ex 4", document.querySelectorAll('ul > li')[2])
+
+// 5. Que ocurre cuando se ejecuta este comando. ¿Por qué?
+// let elementos = document.querySelector('li');
+// elementos[2].remove();
+//
+// there's 2 commands, not one
+// - first one: gets the first element in the query
+// - second one: attempts to print the value of the index 2 (nonexistant)
+
+// 6. Que ocurre cuando se ejecuta este comando. ¿Por qué?
+// let lista = document.querySelectorAll('ul');
+// lista.remove();
+//
+// again, there's 2 commands, not one
+// - first one: gets a nodelist that contains elements
+// - second one: attempts to call the function remove from nodelist (nonexistant)
+
+// 7. Accede a la lista ul y borra el segundo de sus hijos, utilizando su propiedad children
+document.getElementsByTagName('ul')[0].children[1].remove()
+
+
+// 8. Accede a la lista ul y borra el segundo de sus hijos, utilizando su propiedad childNodes
+Array.from(document.getElementsByTagName('ul')[0].childNodes).filter(node => node.nodeName == "LI")[1].remove()
+
+
+// 9. Completa este código para que se ponga de color azul el hermano siguiente al primer elemento de la lista
+
+// this code CAN'T work, we need an extra li
+{
+    let li = document.createElement("li")
+    li.textContent = "NO HAY MAS LIs"
+    li.className = "item"
+    document.getElementsByTagName('ul')[0].appendChild(li)
+}
+////////////////////////////////////////////
+
+// annoying code line that cannot be removed
+let element = document.querySelector('li:first-child');
+
+// haskell-like operation to get the next LI
+let getNextLi = (element) => {
+    let next = element.nextSibling
+    return next.nodeName == "LI" ? next : getNextLi(next)
+}
+getNextLi(element).style.color="blue"
+
+
+// 10. Completa este código para que se pongan de color azul él y todos sus hermanos
+
+// change of wording suggestion:
+// Completa este código para que se pongan de color azul el primer elemento de la lista y todos sus hermanos
+
+// annoying code line that cannot be removed
+let elemento = document.querySelector('li:first-child');
+
+// haskell-like operation to get all LIs
+let getAllSiblings = (current, LiList = []) => {
+    let next = current.nextSibling
+    if (current.nodeName == "LI") LiList.push(current)
+    if (next == undefined) return LiList
+    return getAllSiblings(next, LiList)
+}
+getAllSiblings(element).forEach(s => s.style.color="blue")
+
+
+// 11. ¿Por qué no podemos acceder así al atributo 'tema' del #título?
+// let titulo = document.querySelector('#titulo');
+// console.log(titulo.tema);
+
+// First of all, the first line of code is incompatible with exercise 2.
+// We can't access custom attributes this way because it would be stupid (imagine naming an attribute "appendChild").
+
+
+// 12. Saca por consola el valor del atributo 'tema' del #título
+console.log("ex 12", titulo.getAttribute("tema"))
+
+
+// 13. Añade al título el atributo 'cfgs' con valor 'daw'
+titulo.setAttribute("cfgs", "daw")
+
+
+// 14. Cambia el id del h3 sin utilizar el método setAttribute
+
+// Unnecessary change of wording. Wording suggestion:
+// 14. Cambia el id del titulo sin utilizar el método setAttribute
+titulo.id = "title"
+
+
+// 15. ¿Este código añade correctamente la classe 'elem' a los elementos de la lista?
+// let elementos = document.querySelectorAll('.item');
+// for (let elemento of elementos){
+//     elemento.className = "elem";
+// }
+
+// no, it replaces all classes for this one
+
+
+// 16. Añade correctamente a los elementos de la lista la clase "item"
+
+// are you sure this is what you wanted? item class, not element class?
+Array.from(document.getElementsByClassName("item")).map(li => li.classList.add("item"))
+
+
+// 17. ¿Es correcta la salida por consola?
+// let primerElemento= document.querySelectorAll('.item')[0];
+// primerElemento.classList.add("elem");
+// console.log(primerElemento.className);
+
+// The output is always correct, unless there's a bug in the interpreter.
+// i don't understand what is being asked here
+
+
+// 18. Añade un Cuarto Elemento al final de la lista
+
+// *3rd element; remember when you made me remove the other ones?
+{
+    let li = document.createElement("li")
+    li.textContent = "haz ver que este es el cuarto"
+    li.className = "item"
+    document.getElementsByTagName('ul')[0].appendChild(li)
+}
+
+
+// 19. Añade un Elemento Cero al principio de la lista
+{
+    let li = document.createElement("li")
+    li.textContent = "este se ha colado"
+    li.className = "item"
+    document.getElementsByTagName('ul')[0].insertBefore(li, document.querySelector("ul > li"))
+}
+
+
+// 20. Duplica el Tercer Elemento a continuación de éste en la lista
+{
+    let og = document.getElementsByClassName("item")[2]
+    let clone = og.cloneNode(true)
+    document.getElementsByTagName('ul')[0].insertBefore(clone, document.getElementsByClassName("item")[3])
+}
